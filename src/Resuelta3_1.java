f9d63d335e9eda4ad3b59781cf6cdefbf5275389
AngelsGV <angels.not99@gmail.com>
2023-11-02 22:23:27 +0100

    Initial commit

--- src/Resuelta3_1.java
+++ src/Resuelta3_1.java
@@ -7,8 +7,8 @@ public class Resuelta3_1 {
         System.out.println("Introduzca un numero: ");
         int num = sc.nextInt();
         while (num != 0 ){
-            esPar = num % 2 == 0 ? true : false;
-            esPositivo = num >= 0 ? true : false;
+            esPar = num % 2 == 0;
+            esPositivo = num >= 0;
             System.out.println("Es par? "+ esPar +  "/nEs positivo? " + esPositivo );
             System.out.println("Cuadrado: " + num*num);
             System.out.print("Introduzca otro número: ");
