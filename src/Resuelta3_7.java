dbb6f2f583ac21c4dbb755d32428fb1f3572f6c8
AngelsGV <angels.not99@gmail.com>
2023-11-06 00:05:20 +0100

    Initial commit

---
+++ src/Resuelta3_7.java
@@ -0,0 +1,8 @@
+//multiplos de 7 menores de 100
+public class Resuelta3_7 {
+    public static void main(String[] args){
+        for (int i=0; i < 100; i+=7){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Resuelta3_8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Resuelta3_8 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int suma=0;
+        double media;
+        for (int i=1; i <= 10; i++){
+            System.out.println("Escriba un número: ");
+            n=sc.nextInt();
+            suma += n;
+        }
+        media = suma/10;
+        System.out.println("La media es: "+ media);
+    }
+}
